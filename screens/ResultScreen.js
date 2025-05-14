4198e072cf9127f8fec9d9ab2e807e52702cbf23
Amruta Killampalli <ksavarshini@gmail.com>
2025-05-14 12:36:30 -0400

    Initial commit: app setup

---
+++ screens/ResultScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ResultScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Playlist Will Show Here 🎶</Text>
+    </View>
+  );
+}
